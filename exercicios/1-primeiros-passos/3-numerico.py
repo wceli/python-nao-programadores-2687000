d1e16f43abfc6cae7dc1427d2d910ba09be80345
wceli <98104373+wceli@users.noreply.github.com>
2025-05-22 02:16:41 +0000

    Realizando exercício com números

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -4,8 +4,15 @@ altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
 
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
 
 
+print(data_nascimento * 5)
+
 # Realize uma operação entre dados do tipo inteiro e float
+
+print (idade_numerica+altura)
